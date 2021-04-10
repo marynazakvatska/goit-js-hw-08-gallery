e7c5ddf459f4bfc25759d692bbaba9154e0cf5d6
marynazakvatska <maryna.zakvatska@gmail.com>
2021-04-11 00:07:33 +0200

    modalka rabotaet

--- index.js
+++ index.js
@@ -1,105 +1,62 @@
 import images from "./gallery-items.js";
+const gallery = document.querySelector(".js-gallery");
+const lightbox = document.querySelector(".lightbox");
 
-const lightbox = document.querySelector(".js-lightbox");
-const lightboxImage = document.querySelector("img.lightbox__image");
-const currentImage = document.querySelector(`.js-lightbox.is-open`);
-
-console.log(images);
+const lightboxCloseBtn = document.querySelector(
+  `button[data-action="close-lightbox"]`
+);
+const lightBoxImage = document.querySelector("img.lightbox__image");
+/* Создание и рендер разметки по массиву данных и предоставленному шаблону. */
 const makeImgListMarkup = (image) => {
-  // console.log(image)
-  return `<li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="${image.original}"
-  >
-    <img
-      class="gallery__image"
-      src="${image.preview}"
-      data-source="${image.original}"
-      alt="${image.description}"
-    />
-  </a>
-</li>`;
+  return ` <li class="gallery__item">
+      <a
+        class="gallery__link"
+        href="${image.original}"
+      >
+        <img
+          class="gallery__image"
+          src="${image.preview}"
+          data-source="${image.original}"
+          alt="${image.description}"
+        />
+      </a>
+    </li> `;
 };
 console.log(makeImgListMarkup);
-const imgList = document.querySelector(".js-gallery");
-console.log(imgList);
-const makeImgListRows = images.map(makeImgListMarkup).join("");
-console.log(makeImgListRows);
-imgList.insertAdjacentHTML("afterbegin", makeImgListRows);
-
-/* Реализация делегирования на галерее ul.js - gallery и получение url большого изображения. */
-imgList.addEventListener("click", onImgListClick);
-function onImgListClick(evt) {
-  /* evt.preventDefault; */
-  console.log(evt.currentTarget);
-  const isImgListSwach = evt.target.classList.contains("gallery__link");
-  if (!isImgListSwach) {
+const makeImgRawMarkup = images.map(makeImgListMarkup).join("");
+console.log(makeImgRawMarkup);
+gallery.insertAdjacentHTML("afterbegin", makeImgRawMarkup);
+gallery.addEventListener("click", onImageGalleryList);
+lightbox.addEventListener("click", onCloseBtn);
+function onImageGalleryList(e) {
+  e.preventDefault();
+  if (e.target.localName !== "img") {
     return;
   }
-  /* /* Открытие модального окна по клику на элементе галереи.
-   */
-  addIsOpen();
-  setOriginalImg(evt);
-
-  /*  Подмена значения атрибута src элемента img.lightbox__image.
-   */
-  replaceAttributes();
+  addOpenLightboxClass();
+  addAttributes(e);
 }
-
-function addIsOpen() {
-  console.log(lightbox);
+function addOpenLightboxClass() {
   lightbox.classList.add("is-open");
 }
-function setOriginalImg(evt) {
-  console.log(evt.target.dataset.source);
+function addAttributes(evt) {
+  lightBoxImage.src = evt.target.dataset.source;
+  lightBoxImage.alt = evt.target.alt;
 }
-function replaceAttributes() {
-  console.log(lightboxImage);
-
-  lightboxImage.src = `${evt.target.dataset.source}`;
-  lightboxImage.alt = `${evt.target.alt}`;
-}
-
-/* /* Закрытие модального окна по
-клику на кнопку button[data - action= "close-lightbox"]. */
-
-const closeBtn = document.querySelector(`button[data-action]`);
-closeBtn.addEventListener("click", onCloseBtn);
-
-function onCloseBtn({ target }) {
-  if (target.nodeName === "BUTTON" && target.dataset.action) {
-    removeIsOpen();
+buttonEl.addEventListener("click", onCloseBtn);
+function onCloseBtn(e) {
+  const onOverlayCloseBtn = e.target === lightboxCloseBtn;
+  /* console.log(e.currentTarget); */
+  if (onOverlayCloseBtn) {
+    removeLightboxIsOpen();
     removeAtributes();
   }
 }
-function removeIsOpen() {
-  currentImage.classList.remove(`.is-open`);
+function removeLightboxIsOpen() {
+  lightbox.classList.remove("is-open");
 }
-
 /*  Очистка значения атрибута src элемента img.lightbox__image. */
 function removeAtributes() {
-  lightboxImage.src = "";
-  lightboxImage.alt = "";
+  lightBoxImage.src = "";
+  lightBoxImage.alt = "";
 }
-
-/* Очистка значения атрибута src элемента img.lightbox__image.Это необходимо для того,
- чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее. */
-/* 
-function onModalCloseBtn(e) {
-  const onOverlayCloseBtn = e.target === lightboxCloseBtn;
-  console.log(e.currentTarget);
-  if (onOverlayCloseBtn) {
-    removeLightboxClass();
-    removeImageAtributes();
-  }
-}
-// очищає атрибути src i alt
-function removeImageAtributes() {
-  lightboxImage.src = "";
-  lightboxImage.alt = "";
-}
-// видаляє клас модалці
-function removeLightboxClass() {
-  lightbox.classList.remove("is-open");
-} */
